8be6bba82bed5698febc1bca24a079bb5f18b5b9
jfrosty95 <jfrosty95@yahoo.com>
2017-10-27 21:45:43 -0500

    Changed a little bit, not working, can hopefully fix quickly

--- scrapers/rmpScraper.java
+++ scrapers/rmpScraper.java
@@ -1,4 +1,6 @@
-import java.io.IOEcseption;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.lang.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -6,11 +8,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class rmpScraper {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 
 		int currOffset = 0;
 		PrintWriter writer = new PrintWriter("rmplinks.txt", "UTF-8");
-		while(currOffset <= 0) {
+		while(currOffset <= 3960) {
 			String url = "http://www.ratemyprofessors.com/search.jsp?query=university+of+wisconsin+madison&queryoption=HEADER&stateselect=&country=&dept=&queryBy=&facetSearch=&schoolName=&offset=" + Integer.toString(currOffset) + "&max=20";
 			
 			Document doc = Jsoup.connect(url).get();
