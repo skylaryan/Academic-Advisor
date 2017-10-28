992002d5635d07ccf9e3b777aa67371917ed49ee
jfrosty95 <jfrosty95@yahoo.com>
2017-10-27 20:56:36 -0500

    created folder for scrapers as well as initial rmp scraper

---
+++ scrapers/rmpScraper.java
@@ -0,0 +1,32 @@
+import java.io.IOEcseption;
+import java.lang.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class rmpScraper {
+	public static void main(String[] args) {
+
+		int currOffset = 0;
+		PrintWriter writer = new PrintWriter("rmplinks.txt", "UTF-8");
+		while(currOffset <= 0) {
+			String url = "http://www.ratemyprofessors.com/search.jsp?query=university+of+wisconsin+madison&queryoption=HEADER&stateselect=&country=&dept=&queryBy=&facetSearch=&schoolName=&offset=" + Integer.toString(currOffset) + "&max=20";
+			
+			Document doc = Jsoup.connect(url).get();
+			Elements links = doc.select("a[href]:contains(ShowRatings)"); 
+
+			for(Element link : links) {
+			 	writer.println(link.attr("abs:href"));
+			}
+
+
+
+			currOffset += 20;
+		}
+		writer.close();
+
+
+
+	}
+}
