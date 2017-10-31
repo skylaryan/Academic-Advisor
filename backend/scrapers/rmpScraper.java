9441e4d18a6cd1bbeac9cc7e98cd16297534f891
Utkarsh Jain <utkarsh@utkarshs-MacBook-Air.local>
2017-10-31 17:09:14 -0500

    Extracting tags for each professor

--- backend/scrapers/rmpScraper.java
+++ backend/scrapers/rmpScraper.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
@@ -22,51 +17,62 @@ class Professor
     String name;
     double rating;
     double diff;
+    List< String > tag;
 }
 public class rmpScraper {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        /*
-         int currOffset = 0;
-         PrintWriter writer = new PrintWriter("rmplinks.txt", "UTF-8");
-         while(currOffset <= 3960) {
-         String url = "http://www.ratemyprofessors.com/search.jsp?query=university+of+wisconsin+madison&queryoption=HEADER&stateselect=&country=&dept=&queryBy=&facetSearch=&schoolName=&offset=" + Integer.toString(currOffset) + "&max=20";
-         
-         Document doc = Jsoup.connect(url).get();
-         Elements links = doc.select("a[href]");
-         
-         for(Element link : links) {
-         if(link.text().contains("PROFESSOR"))
-         writer.println(link.attr("abs:href"));
-         }
-         currOffset += 20;
-         }
-         writer.close();
-         */
+        
+        
+        int currOffset = 0;
+        PrintWriter writer = new PrintWriter("rmplinks.txt", "UTF-8");
+        // In this loop, we query ratemyprofessors for UW-Madison professors and put a link to each professors page in .txt called rmplinks.txt
+        while(currOffset <= 3960) {
+            String url = "http://www.ratemyprofessors.com/search.jsp?query=university+of+wisconsin+madison&queryoption=HEADER&stateselect=&country=&dept=&queryBy=&facetSearch=&schoolName=&offset=" + Integer.toString(currOffset) + "&max=20";
+            
+            Document doc = Jsoup.connect(url).get();
+            Elements links = doc.select("a[href]");
+            
+            for(Element link : links) {
+                if(link.text().contains("PROFESSOR"))
+                    writer.println(link.attr("abs:href"));
+            }
+            currOffset += 20;
+        }
+        writer.close();
+        
+        // for each link in rmplinks.txt, we extract information about each professor
         ArrayList<Professor> arr = new ArrayList();
         try (BufferedReader br = new BufferedReader(new FileReader("rmplinks.txt"))) {
             String line;
             while ((line = br.readLine()) != null)
             {
                 Document doc = Jsoup.connect(line).get();
-                Professor s = new Professor();
-                Elements x = doc.getElementsByClass("profname");
-                Elements q = doc.getElementsByClass("grade");
+                // getElements by class returns an Elements object with information of each class with the specified name in the HTML file
+                Elements pname = doc.getElementsByClass("profname");
+                Elements rating = doc.getElementsByClass("grade"); // difficulty and professor rating both have class=grade
+                Elements tags = doc.getElementsByClass("tag-box-choosetags");
                 
-                List<String> y = new LinkedList<String>();
-                List<String> z = new LinkedList<String>();
+                List<String> pn = new LinkedList<String>();
+                List<String> rat = new LinkedList<String>();
+                List<String> proftag = new LinkedList<String>();
                 
                 
-                y = x.eachText();
-                z = q.eachText();
-                for(int i=0;i<y.size();i++)
+                // eachText returns a list of strings of the content of the Elements object
+                pn = pname.eachText();
+                rat = rating.eachText();
+                proftag = tags.eachText();
+                Professor temp = new Professor();
+                temp.name = pn.get(0);
+                temp.rating = Float.parseFloat(rat.get(0));
+                temp.diff = Float.parseFloat(rat.get(2));
+                temp.tag = proftag;
+                arr.add(temp);
+                System.out.println(temp.name+" "+temp.rating+" "+temp.diff);
+                for(int j=0;j<proftag.size();j++)
                 {
-                    Professor temp = new Professor();
-                    temp.name = y.get(i);
-                    temp.rating = Float.parseFloat(z.get(i));
-                    temp.diff = Float.parseFloat(z.get(i+2));
-                    arr.add(temp);
-                    System.out.println(temp.name+" "+temp.rating+" "+temp.diff);
+                    System.out.println(temp.tag.get(j));
                 }
+                System.out.println();
             }
             /*
              for(int i=0;i<arr.size();i++)
