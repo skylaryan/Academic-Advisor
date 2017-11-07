56467feecdda11b455fdbbc145004233dc2bfd32
JamesLiu37 <jtliu@wisc.edu>
2017-11-06 21:15:58 -0600

    Updated to scrape max page count
    Instead of hardcoding a max page value

--- backend/scrapers/rmpScraper.java
+++ backend/scrapers/rmpScraper.java
@@ -26,11 +26,17 @@ public class rmpScraper {
         int currOffset = 0;
         PrintWriter writer = new PrintWriter("rmplinks.txt", "UTF-8");
         // In this loop, we query ratemyprofessors for UW-Madison professors and put a link to each professors page in .txt called rmplinks.txt
-        while(currOffset <= 3960) {
-            String url = "http://www.ratemyprofessors.com/search.jsp?query=university+of+wisconsin+madison&queryoption=HEADER&stateselect=&country=&dept=&queryBy=&facetSearch=&schoolName=&offset=" + Integer.toString(currOffset) + "&max=20";
+        String url = "http://www.ratemyprofessors.com/search.jsp?query=university+of+wisconsin+madison&queryoption=HEADER&stateselect=&country=&dept=&queryBy=&facetSearch=&schoolName=&offset=" + Integer.toString(currOffset) + "&max=20";
             
-            Document doc = Jsoup.connect(url).get();
-            Elements links = doc.select("a[href]");
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");
+        
+		Elements resultCount = docScrapeLinks.getElementsByClass("result-count");
+		String[] parsed = resultCount.eachText().get(1).split(" ");
+		int maxPage = Integer.parseInt(parsed[parsed.length - 2]);        
+        
+        while(currOffset <= maxPage) {
+            
             
             for(Element link : links) {
                 if(link.text().contains("PROFESSOR"))
