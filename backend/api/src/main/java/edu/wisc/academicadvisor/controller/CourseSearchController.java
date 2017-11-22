8769783843fedc443b0f025d8bb216959d73df61
Tyler O'Connell <ttoconnell@wisc.edu>
2017-11-22 15:20:37 -0600

    draft of api service

---
+++ backend/api/src/main/java/edu/wisc/academicadvisor/controller/CourseSearchController.java
@@ -0,0 +1,46 @@
+package edu.wisc.academicadvisor.controller;
+
+import edu.wisc.academicadvisor.model.Course;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class CourseSearchController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @RequestMapping("/courses")
+    public List<Course> courses(@RequestParam(value="breadth", defaultValue="") String breadth) {
+        List<Map<String, Object>> rows;
+        List<Course> courses = new ArrayList<>();
+        try {
+            if (!breadth.isEmpty()) {
+                breadth = breadth.replace("-", "' OR '"); // .replace("%20", " ")
+                rows = jdbcTemplate.queryForList("SELECT * FROM mergedcourses WHERE breadth=" + breadth);
+            } else rows = jdbcTemplate.queryForList("SELECT * FROM mergedcourses");
+
+            for (Map row : rows) {
+                Course course = new Course((String)row.get("course"),
+                                            (Integer)row.get("section"),
+                                            (String)row.get("title"),
+                                            (Integer)row.get("numCredits"),
+                                            ((String)row.get("breadth")).split("|"),
+                                            (String)row.get("professor"),
+                                            (Double)row.get("professorRating"),
+                                            (String)row.get("description"),
+                                            ((String)row.get("schedule")).split("|"));
+                courses.add(course);
+            }
+        } catch (Exception ex) { ex.printStackTrace(); }
+        return courses;
+    }
+}
---
+++ backend/api/src/main/java/edu/wisc/academicadvisor/model/Course.java
@@ -0,0 +1,71 @@
+package edu.wisc.academicadvisor.model;
+
+public class Course {
+
+    // properties
+    private String course; // CS200
+    private int section; // 001
+    private String title; // Introduction to Programming
+    private int numCredits; // 3
+    private String[] breadth; // {"Natural Sciences"}
+    private String professor; // John Doe
+    private double professorRating; // 4.9
+    private double[][] gradeHistory; // list of last four semesters of grade info, each with avg gpa, percent A, AB, ...
+    private String description; // This is a class where you will learn how to program.
+    private String[] schedule; // MWF 2:30-3:45 & F 4-5 as {"14:30-15:45","","14:30-15:45","","14:30-15:45|16:00-17:00"}
+
+    public Course(String course, int section, String title, int numCredits, String[] breadth, String professor,
+                  double professorRating, /*double[][] gradeHistory, */String description, String[] schedule) {
+        this.course = course;
+        this.section = section;
+        this.title = title;
+        this.numCredits = numCredits;
+        this.breadth = breadth;
+        this.professor = professor;
+        this.professorRating = professorRating;
+        //this.gradeHistory = gradeHistory; TODO
+        this.description = description;
+        this.schedule = schedule;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumCredits() {
+        return numCredits;
+    }
+
+    public String[] getBreadth() {
+        return breadth;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public double getProfessorRating() {
+        return professorRating;
+    }
+
+    /*public double[][] getGradeHistory() {
+        return gradeHistory;
+    }*/
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String[] getSchedule() {
+        return schedule;
+    }
+
+}
