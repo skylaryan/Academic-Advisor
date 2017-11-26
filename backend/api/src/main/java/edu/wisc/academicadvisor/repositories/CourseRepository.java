bb312a00124447f0bcc1f4be430e2ae31bd724d8
Tyler O'Connell <ttoconnell@wisc.edu>
2017-11-26 02:09:44 -0500

    working mysql calls from spring boot

--- backend/api/src/main/java/edu/wisc/academicadvisor/repositories/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.wisc.academicadvisor.repositories;
-
-import edu.wisc.academicadvisor.model.Course;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepository extends CrudRepository<Course, String> {
-
-}
