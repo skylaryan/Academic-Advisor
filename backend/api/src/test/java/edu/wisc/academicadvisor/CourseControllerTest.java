7a2f0414bf5a2f32a9f3bbda6761c02405d98520
jfrosty95 <jfrosty95@yahoo.com>
2017-11-26 23:05:45 -0600

    api course controller test made much more verbose

--- backend/api/src/test/java/edu/wisc/academicadvisor/CourseControllerTest.java
+++ backend/api/src/test/java/edu/wisc/academicadvisor/CourseControllerTest.java
@@ -34,4 +34,101 @@ public class CourseControllerTest {
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(jsonPath("$.content").isEmpty());
     }
+
+    @Test
+    public void goodCreditAmountShouldReturnResults() throws Exception {
+
+        this.mockMvc.perform(get("/courses").param("credits", "3"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isNotEmpty());
+    }
+
+    @Test
+    public void badCreditAmountShouldReturnNoResults() throws Exception {
+
+        this.mockMvc.perform(get("/courses").param("credits", "69"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isEmpty());
+    }
+
+    @Test
+    public void goodDepartmentShouldReturnResults() throws Exception {
+
+        this.mockMvc.perform(get("/courses").param("department", "ZOOLOGY"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isNotEmpty());
+
+    }
+
+    @Test
+    public void invalidDepartmentShouldReturnNoResults() throws Exception {
+
+        this.mockMvc.perform(get("/courses").param("department", "ROCKET SURGERY"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isEmpty());
+    }
+
+    @Test
+    public void goodNumberShouldReturnResults() throws Exception {
+
+        this.mockMvc.perform(get("/courses").param("number", "200"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isNotEmpty();
+    }
+
+    @Test
+    public void badNumberShouldReturnNoResults() throws Exception {
+        // should be noted that if the university starts offering courses at this high a level
+        // this test will fail. I, however, do not see that happening
+        this.mockMvc.perform(get("/courses").param("number", "69420666"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isEmpty());
+    }
+
+    @Test
+    public void goodBusyShouldReturnResults() throws Exception {
+        this.mockMvc.perform(get("/courses").param("busy", "13:00-15:00//10:00-11:00//13:00-15:00////13:00-15:00"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isNotEmpty());
+    }
+
+    @Test void alwaysBusyShouldReturnNoResults() throws Exception {
+        this.mockMvc.perform(get("/courses").param("busy", "0:00-23:59//0:00-23:59//0:00-23:59//0:00-23:59//0:00-23:59"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isEmpty());
+    }
+
+    @Test void goodTagsShouldReturnResults() throws Exception {
+        this.mockMvc.perform(get("/courses").param("tags", "plant-epistemology-computer"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isNotEmpty());
+    }
+
+    @Test void badTagsShouldReturnNoResults() throws Exception {
+        this.mockMvc.perform(get("/courses").param("tags", "thisisabadstring-loremipsumamiright-andhisnameisjohncena"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isEmpty());
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
